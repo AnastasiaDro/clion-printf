670cd488053cf785602b635e58fea111cf538e31
Anastsia Dro <adrogunova@gmail.com>
2021-02-02 16:26:43 +0300

    normed ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -11,22 +11,21 @@
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
 
-int ft_printf(const char *format_str, ...)
+int	ft_printf(const char *format_str, ...)
 {
-	va_list 		v_list;
-	int 			res_len;
-	char 			*p;
+	va_list			v_list;
+	int				res_len;
+	char			*p;
 	t_print_flags	*print_struct;
 
 	print_struct = NULL;
 	va_start(v_list, format_str);
 	res_len = 0;
 	p = (char *)format_str;
-	while(*p)
+	while (*p)
 	{
-		if(*p == '%' && *(p++) != '\0')
+		if (*p == '%' && *(p++) != '\0')
 		{
 			print_struct = ft_create_struct();
 			ft_fill_struct(print_struct, res_len, &p, &v_list);
@@ -34,11 +33,7 @@ int ft_printf(const char *format_str, ...)
 			free(print_struct);
 		}
 		else
-		{ 
-		ft_putchar_fd(*p, 1);
-		res_len++;
-		p++;
-		}
+			ft_just_print(&p, &res_len);
 	}
 	va_end(v_list);
 	return (res_len);
